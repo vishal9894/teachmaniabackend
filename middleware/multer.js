3829976037a4db36c6ddffb7fdbafb542aee73ab
vishal9894 <vishalkumar662002@gmail.com>
2026-02-20 15:16:21 +0530

    fix booket

--- middleware/multer.js
+++ middleware/multer.js
@@ -13,7 +13,7 @@ const s3 = new S3Client({
 
 const storage = multerS3({
   s3,
-  bucket: process.env.AWS_BUCKET_NAME,
+  bucket: process.env.AWS_BUCKET_NAME || "",
   acl: "public-read",
   contentType: multerS3.AUTO_CONTENT_TYPE,
 
